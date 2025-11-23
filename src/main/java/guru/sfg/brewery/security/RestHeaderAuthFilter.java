2d2e8d4c0108f227a2d7e43f2979a984d6a7e8f3
Błażej Knie <blazejknie@gmail.com>
2025-11-23 12:12:51 +0100

    create custom Authentication Filter

---
+++ src/main/java/guru/sfg/brewery/security/RestHeaderAuthFilter.java
@@ -0,0 +1,42 @@
+package guru.sfg.brewery.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+@Slf4j
+public class RestHeaderAuthFilter extends AbstractAuthenticationProcessingFilter {
+
+    public RestHeaderAuthFilter(RequestMatcher requiresAuthenticationRequestMatcher) {
+        super(requiresAuthenticationRequestMatcher);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws AuthenticationException, IOException, ServletException {
+        String username = getUsername(httpServletRequest);
+        String password = getPassword(httpServletRequest);
+        log.debug("Authenticating User: " + username + "/" + password);
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
+
+        return this.getAuthenticationManager().authenticate(token);
+    }
+
+    private String getPassword(HttpServletRequest httpServletRequest) {
+        return Optional.ofNullable(httpServletRequest.getHeader("Api-Secret")).orElse("");
+    }
+
+    private String getUsername(HttpServletRequest request) {
+        return Optional.ofNullable(request.getHeader("Api-Key")).orElse("");
+    }
+
+}
+
--- src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerIT.java
@@ -2,8 +2,16 @@ package guru.sfg.brewery.web.controllers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
@@ -25,5 +33,16 @@ public class BeerRestControllerIT extends BaseIT{
     void findBeerByUPC() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/beerUpc/012345678905"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void deleteBeerByIdWithAuth() throws Exception {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Api-Key", "spring");
+        httpHeaders.add("Api-Secret", "guru");
+        mockMvc.perform(delete("/api/v1/beer/36ebf408-4226-4378-94bf-fa89a53033a4").headers(httpHeaders))
+                .andExpect(status().isNoContent());
+
+        verify(beerRepository).deleteById(UUID.fromString("36ebf408-4226-4378-94bf-fa89a53033a4"));
     }
 }
